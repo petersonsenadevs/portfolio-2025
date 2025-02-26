837e211110315e9af23799b8c5a21204fc30164f
Peterson Sena <petesonsena24@gmail.com>
2025-02-26 10:23:25 +0100

    About, translations

--- src/i18n/translations.ts
+++ src/i18n/translations.ts
@@ -27,6 +27,43 @@ export const translations = {
       es: "Contacto",
     },
   },
+  hero: {
+    title: {
+      es: "Peterson Sena",
+      en: "Peterson Sena",
+    },
+    subtitle: {
+      es: "Desarrollador",
+      en: "Developer",
+    },
+    buttons: {
+      contact: {
+        en: "Contact Me",
+        es: "Contáctame",
+      },
+      projects: {
+        en: "Projects",
+        es: "Proyectos",
+      },
+    },
+  },
+
+  about: {
+    title: {
+      en: "About Me",
+      es: "Sobre Mí",
+    },
+   
+    description: {
+      en: "What can I tell you! I am a passionate developer from Dominican Republic, living in Spain. Since I was a child I have been fascinated by technology and programming, and nowadays I enjoy creating web solutions and applications. I'm always learning, exploring new tools and looking to improve in every project. I love taking on challenges and working on innovative ideas.",
+      es: "¡Que os cuento! Soy un apasionado desarrollador de República Dominicana, residente en España. Desde pequeño me ha fascinado la tecnología y la programación, y hoy en día disfruto creando soluciones web y aplicaciones. Siempre estoy aprendiendo, explorando nuevas herramientas y buscando mejorar en cada proyecto. Me encanta asumir desafíos y trabajar en ideas innovadoras",
+    },
+    description2: {
+      en: "I focus on blending technology and creativity to develop efficient and functional solutions. My goal is to ensure that every project not only runs smoothly but also delivers an exceptional user experience.",
+      
+      es: "Me enfoco en combinar tecnología y creatividad para desarrollar soluciones eficientes y funcionales. Mi objetivo es asegurar que cada proyecto no solo funcione sin problemas, sino que también ofrezca una experiencia de usuario excepcional.",
+    },
+  },
 
   footer :{
     rights: {
